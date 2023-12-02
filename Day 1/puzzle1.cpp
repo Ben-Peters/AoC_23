3df1516bab9ed6aab134965e376f4718d9d01e8a
Peters, Benjamin <Benjamin.Peters@viasat.com>
2023-12-01 22:44:56 -0500

    part 2

--- Day 1/puzzle1.cpp
+++ Day 1/puzzle1.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 const string FILENAME = "puzzle1.input.txt";
 
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
 int main () {
   string line;
   ifstream myfile (FILENAME);
@@ -13,8 +21,24 @@ int main () {
   {
     while ( getline (myfile,line) )
     {
-      //cout << line << '\n';
-      string digits = regex_replace(line, regex("(\\D)"), "");
+      cout << line << '\n';
+      string short_line;
+        for (int i = 1; i <= line.length(); i++){
+            short_line += line.substr(i-1,1);
+            replace(short_line, "one", "o1e");
+            replace(short_line, "two", "t2o");
+            replace(short_line, "three", "t3e");
+            replace(short_line, "four", "f4r");
+            replace(short_line, "five", "f5e");
+            replace(short_line, "six", "s6x");
+            replace(short_line, "seven", "s7n");
+            replace(short_line, "eight", "e8t");
+            replace(short_line, "nine", "n9e");
+            //replace(short_line, "zero", "0");
+
+        }
+      cout << short_line << endl;
+      string digits = regex_replace(short_line, regex("(\\D)"), "");
       string first_digit = digits.substr(0,1);
       //cout << first_digit;
       string last_digit = digits.substr(digits.length()-1, 1);
