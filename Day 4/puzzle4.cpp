56262d51b77958da00f4e2709d9e6c95e606f085
Peters, Benjamin <Benjamin.Peters@viasat.com>
2023-12-05 14:26:07 -0500

    Day 4 Part 2

--- Day 4/puzzle4.cpp
+++ Day 4/puzzle4.cpp
@@ -13,25 +13,31 @@ int main(){
   string line;
   ifstream myfile (FILENAME);
   int sum = 0;
+  int cards [INPUT_GAMES][2][MINE];
+  int num_copies[INPUT_GAMES];
+  for(int i=0;i<INPUT_GAMES;i++){num_copies[i]=1;}
   if (myfile.is_open())
   {
     for (int i = 0; i < INPUT_GAMES; i++){
         int winning_nums [WINNERS] = {};
         int my_nums [MINE] = {};
         getline (myfile,line);
-        printf("Card %d: ", i+1);
+        //printf("Card %d: ", i+1);
         line = line.substr(10);
         for(int j = 0; j < WINNERS; j++){
             winning_nums[j] = stoi(line.substr(0,2));
+            cards [i][0][j] = stoi(line.substr(0,2));
             line = line.substr(3);
         }
         line = line.substr(2);
         for(int j = 0; j < MINE; j++){
             my_nums[j] = stoi(line.substr(0,2));
+            cards [i][1][j] = stoi(line.substr(0,2));
             if(line.length() >= 3)
                 line = line.substr(3);
         }
 
+#ifdef PART_1
         uint points = 0;
         for (int j = 0; j < WINNERS; j++){
             for (int k = 0; k < MINE; k++){
@@ -48,6 +54,27 @@ int main(){
         sum += points;
         printf("\n");
     }
-  }
+#else
+    }
+    for (int i = 0; i < INPUT_GAMES; i++){
+        int matches = 0;
+        for (int j = 0; j < WINNERS; j++){
+            for (int k = 0; k < MINE; k++){
+                if (cards[i][0][j] == cards[i][1][k]){
+                    matches ++;
+                }
+            }
+        }
+        for (int j = 1; j <= matches; j++){
+            if(i+j >= INPUT_GAMES){
+                break;
+            }
+            num_copies[i+j] += 1 * num_copies[i];
+        }
+        printf("Card %d: (%d, %d)\n", i+1, num_copies[i], matches);
+        sum += num_copies[i];
+    }
+  #endif
+}
   printf("SUM: %d\n", sum);
 }
