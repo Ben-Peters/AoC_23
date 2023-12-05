ff73423e6dcee5f5e10491b6a858e2653a29767e
Peters, Benjamin <Benjamin.Peters@viasat.com>
2023-12-05 13:03:14 -0500

    Day 3 part 2

--- Day 3/puzzle3.cpp
+++ Day 3/puzzle3.cpp
@@ -6,6 +6,25 @@ using namespace std;
 const string FILENAME = "puzzle3.input.txt";
 const int INPUT_COL = 140;
 const int INPUT_ROW = 140;
+//#define PART_1 1;
+
+int find_number(char grid[][INPUT_COL], int i, int j){
+    int start;
+    int num  = 0;
+    for(start = j; (grid[i][j] >= 48 && grid[i][j] <= 57); j--){
+        if (j < 0){
+            break;
+        }
+        start = j;
+    }
+    for(j = start; (grid[i][j] >= 48 && grid[i][j] <= 57); j++){
+        if(j >= INPUT_COL){
+            break;
+        }
+        num = (num * 10) + grid[i][j] - 48;;
+    }
+    return num;
+}
 
 int main () {
   string line;
@@ -25,6 +44,7 @@ int main () {
   }
   int number = 0;
   bool adjacent = false;
+  #ifdef PART_1
   for (int i = 0; i < INPUT_ROW; i++){
     for (int j = 0; j < INPUT_COL; j++){
         if (grid[i][j] >= 48 && grid[i][j] <= 57){
@@ -63,5 +83,59 @@ int main () {
             adjacent = false;
     printf("\n");
   }
+  #else
+    for (int i = 0; i < INPUT_ROW; i++){
+    for (int j = 0; j < INPUT_COL; j++){
+        if (grid[i][j] == 42){
+            int num_parts = 0;
+            int part_loc [2][2];
+            bool same_num = false;
+            for (int y_offset = -1; y_offset <= 1; y_offset++){
+                for (int x_offset = -1;x_offset <= 1; x_offset++){
+                    if ((i + y_offset < 0 || i + y_offset >= INPUT_ROW) || (j + x_offset < 0 || j + x_offset >= INPUT_COL)){
+                        continue;
+                    }
+                    char c = grid[i + y_offset][j + x_offset];
+                    if (c >= 48 && c <= 57){
+                        if (same_num){
+                            continue;
+                        }
+                        same_num = true;
+                        if (num_parts < 2) {
+                            part_loc [num_parts][0] = i + y_offset;
+                            part_loc [num_parts][1] = j + x_offset;
+                        }
+                        num_parts ++;
+                    } else {
+                        same_num = false;
+                    }
+                }
+                same_num = false;
+            }
+            if (num_parts == 2){
+                int num1 = find_number(grid, part_loc[0][0], part_loc[0][1]);
+                int num2 = find_number(grid, part_loc[1][0], part_loc[1][1]);
+                sum += (num1 * num2);
+            }
+            printf("*");
+        } else {
+            if (grid[i][j] >= 48 && grid[i][j] <= 57){
+                printf("%c", grid[i][j]);
+            } else {
+            printf(" ");
+            }
+        }
+    }
+    if (number && adjacent){
+                printf("%d",number);
+                sum += number;
+            } else if (number){
+                printf("~~~");
+            }
+            number = 0;
+            adjacent = false;
+    printf("\n");
+  }
+  #endif
   printf("SUM: %d", sum);
 }
