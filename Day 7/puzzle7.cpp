70e1e36c09c567a37ddd9ac7091c76f7bf793867
Peters, Benjamin <Benjamin.Peters@viasat.com>
2023-12-07 14:20:54 -0500

    Day 7 Part 1

---
+++ Day 7/puzzle7.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <list>
+using namespace std;
+const string FILENAME = "puzzle7.input.txt";
+const int NUM_HANDS = 1000;
+#define PART_1 1
+
+enum hand_type {
+    FIVE_OF_KIND = 7,
+    FOUR_OF_KIND = 6,
+    FULL_HOUSE = 5,
+    THREE_OF_KIND = 4,
+    TWO_PAIR = 3,
+    ONE_PAIR = 2,
+    HIGH_CARD = 1,
+    NO_TYPE
+};
+
+struct hand {
+    string cards;
+    int bid;
+    hand_type h_type;
+    hand(string c, int b, hand_type t){
+        cards = c;
+        bid = b;
+        h_type = t;
+    }
+    hand(){
+        cards="";
+        bid=0;
+        h_type=NO_TYPE;
+    }
+};
+
+array<hand, NUM_HANDS> hands;
+
+hand_type findType(string hand){
+    int unq_chars = 1;
+    string seen_chars = hand.substr(0,1);
+    for (int i=1; i < 5; i++){
+        if(seen_chars.find(hand.substr(i,1)) == string::npos){
+            seen_chars.append(hand.substr(i,1));
+            unq_chars++;
+        }
+    }
+    int max_count = 0;
+    for (int i=0; i<unq_chars; i++){
+        char search_c = seen_chars[i];
+        max_count = max(max_count, (int)count(hand.begin(), hand.end(), search_c));
+    }
+    if(unq_chars == 1){
+        return FIVE_OF_KIND;
+    }else if (unq_chars == 2){
+        // four of kind or full house
+        if(max_count == 4){
+            return FOUR_OF_KIND;
+        }else{
+            return FULL_HOUSE;
+        }
+    }else if (unq_chars == 3){
+        // 3 of kind or 2 pair
+        if(max_count == 3){
+            return THREE_OF_KIND;
+        }else{
+            return TWO_PAIR;
+        }
+    }else if (unq_chars == 4){
+        return ONE_PAIR;
+    }else if (unq_chars == 5){
+        return HIGH_CARD;
+    }
+    return NO_TYPE;
+}
+
+int card_to_int(char c){
+    switch (c) {
+    case 'A':
+        return 14;
+        break;
+    case 'K':
+        return 13
+        ;break;
+    case 'Q':
+        return 12;
+        break;
+    case 'J':
+        return 11;
+        break;
+    case 'T':
+        return 10;
+        break;
+    case '9':
+        return 9;
+        break;
+    case '8':
+        return 8;
+        break;
+    case '7':
+        return 7;
+        break;
+    case '6':
+        return 6;
+        break;
+    case '5':
+        return 5;
+        break;
+    case '4':
+        return 4;
+        break;
+    case '3':
+        return 3;
+        break;
+    case '2':
+        return 2;
+        break;
+    case '1':
+        return 1;
+        break; 
+    }
+    return -1;
+}
+
+bool compare_hands (hand h1, hand h2){
+    // should return whether h2 beats h1
+    if(h1.h_type < h2.h_type){
+        return true;
+    }else if( h1.h_type == h2.h_type){
+        for(int i=0; i<5; i++){
+            if(card_to_int(h1.cards[i]) < card_to_int(h2.cards[i])){
+                return true;
+            } else if(card_to_int(h1.cards[i]) > card_to_int(h2.cards[i])){
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+  string line;
+  ifstream myfile (FILENAME);
+  if (myfile.is_open())
+  {
+    for(int i=0; i<NUM_HANDS; i++){
+        getline (myfile,line);
+        hands[i] = hand(
+            line.substr(0,5),
+            stoi(line.substr(6)),
+            findType(line.substr(0,5)));
+    }
+  }
+  sort(hands.begin(),hands.end(),compare_hands);
+  int sum = 0;
+  for(int i=0; i<NUM_HANDS; i++){
+    printf("%s\t%d\t%d\n",hands[i].cards.c_str(), hands[i].bid, hands[i].h_type);
+    sum += hands[i].bid * (i+1);
+  }
+  printf("SUM: %d", sum);
+}
