725895cdf5a93670165218bb6a9278204e9eddce
Peters, Benjamin <Benjamin.Peters@viasat.com>
2023-12-11 15:30:58 -0500

    Day 8 Part 2

--- Day 8/puzzle8.cpp
+++ Day 8/puzzle8.cpp
@@ -3,13 +3,13 @@
 #include <string>
 #include <regex>
 #include <list>
+#include <numeric>
 using namespace std;
 const string FILENAME = "puzzle8.input.txt";
 const int NUM_NODES = 726;
 const string START_NODE = "AAA";
 const string END_NODE = "ZZZ";
-//#define PART_2 1
-
+#define PART_2 1
 struct Node{
     string name;
     Node* left;
@@ -20,8 +20,6 @@ struct Node{
         right = r;
     }
 };
-
-const Node* startPoint = (Node*) malloc(sizeof(Node));
 
 int find_node_from_str(string node, string nodes_arr [NUM_NODES][3]){
     for(int i = 0; i < NUM_NODES; i++){
@@ -39,7 +37,8 @@ int main(){
   string str_nodes [NUM_NODES][3] = {};
   Node* nodes [NUM_NODES] = {};
   Node* cur_node;
-  int num_steps = 0;
+  vector<Node*> cur_nodes_list;
+  long long num_steps = 0;
   if (myfile.is_open())
   {
     getline(myfile,line);
@@ -53,9 +52,15 @@ int main(){
         str_nodes [i][2] = line.substr(12,3);
         nodes[i] = (Node*) malloc(sizeof(Node));
         nodes[i]->name = str_nodes[i][0];
+        #ifdef PART_2 
+        if(nodes[i]->name[2] == 'A'){
+            cur_nodes_list.push_back(nodes[i]);
+        }
+        #else
         if(nodes[i]->name == START_NODE){
             cur_node = nodes[i];
         }
+        #endif
     }
     for(int i=0; i<NUM_NODES; i++){
         auto node_str = str_nodes[i];
@@ -65,15 +70,68 @@ int main(){
         nodes[i]->right = r_node;
     }
   }
+  int num_done = 0;
+  int *loop_size = (int*) calloc(cur_nodes_list.size(), sizeof(int));
+  bool loop_found [6] = {0,0,0,0,0,0};
+  #ifdef PART_2
+  while(num_done < cur_nodes_list.size()){
+  #else
   while(cur_node->name != END_NODE){
+  #endif
+
     if(directions[num_steps%directions.length()] == 'L'){
+        #ifdef PART_2
+        for(int i=0; i < cur_nodes_list.size(); i++){
+            cur_nodes_list[i] = cur_nodes_list[i]->left;
+            if(cur_nodes_list[i]->name[2] == 'Z' && loop_found[i] == false){
+                printf("Sarting Node %d found a solution after %lld steps\n", i, num_steps+1);
+                loop_size[num_done] = num_steps;
+                loop_found[i]=true;
+                num_done++;
+            }
+        }
+        #else
         cur_node = cur_node->left;
         printf("Going LEFT to %s\n", cur_node->name.c_str());
+        #endif
     }else {
+        #ifdef PART_2
+        for(int i=0; i < cur_nodes_list.size(); i++){
+            cur_nodes_list[i] = cur_nodes_list[i]->right;
+            if(cur_nodes_list[i]->name[2] == 'Z' && loop_found[i] == false){
+                printf("Sarting Node %d found a solution after %lld steps\n", i, num_steps+1);
+                loop_size[num_done] = num_steps;
+                loop_found[i]=true;
+                num_done++;
+            }
+        }
+        #else
         cur_node = cur_node->right;
         printf("Going RIGHT to %s\n", cur_node->name.c_str());
+        #endif
     }
     num_steps ++;
+    if(num_done>2){
+       // printf("%lld: %d done\n", num_steps, num_done);
+    }
   }
-  printf("It took %d steps to get from AAA to ZZZ\n", num_steps);
+  long long least_mult[6];
+  bool done = false;
+  while(!done){
+    done = true;
+    for(int i=0; i<6; i++){
+        least_mult[i] += loop_size[i];
+        while(least_mult[i] < least_mult[5]){
+            least_mult[i] += loop_size[i];
+        }
+    }
+    for(long long n : least_mult){
+        done &= (n == least_mult[0]);
+        //printf("%lld, ",n);
+    }
+    if((least_mult[0] % 1000000000)==0) {
+        printf("%lld\n", least_mult[0]);
+    }
+  }
+  printf("It took %lld steps\n", least_mult[0]);
 }
